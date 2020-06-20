f5db2a1414ebc1e7038f0e76041ee14ed5b11141
alishakavene <akmunywoki@gmail.com>
2020-06-20 14:50:37 +0300

    remove one quote array

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -15,11 +15,7 @@ export const quotes = [
     description: '"Let us make one thing clear you will never have this -Loyalty. You can not buy it, You can not own it, You can not force it, it comes only out of love and the respect for the people you believe in."',
     id:'Grishon'
   },
-  {
-    name:'Rebekah Mikaelson',
-    description:'"No matter how big your Empire become it is nothing if you have nobody to share it with."',
-    id:'Blake'
-  }
+ 
 ];
 
 
